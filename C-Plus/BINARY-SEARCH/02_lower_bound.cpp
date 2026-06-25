4a687759abd7ad5e67ad795788eb304e0b5f88c9
sachin kandpal <sachinkandpal0@gmail.com>
2026-06-25 10:07:44 +0530

    finding the lower bound in the rray

--- C-Plus/BINARY-SEARCH/02_lower_bound.cpp
+++ C-Plus/BINARY-SEARCH/02_lower_bound.cpp
@@ -36,3 +36,62 @@ int main() {
 
     return 0;
 }
+
+
+
+// // optimal solution
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Function to find the Lower Bound using Binary Search
+// int lowerBound(vector<int>& arr, int x) {
+
+//     int n = arr.size();
+
+//     int low = 0;
+//     int high = n - 1;
+
+//     // Default answer
+//     // If no element is >= x, return n
+//     int ans = n;
+
+//     // Binary Search
+//     while (low <= high) {
+
+//         // Find the middle index
+//         int mid = low + (high - low) / 2;
+
+//         // Current element can be a lower bound
+//         if (arr[mid] >= x) {
+
+//             ans = mid;
+
+//             // Search on the left side
+//             // to find an even smaller index
+//             high = mid - 1;
+//         }
+//         else {
+
+//             // Search on the right side
+//             low = mid + 1;
+//         }
+//     }
+
+//     return ans;
+// }
+
+// int main() {
+
+//     // Sorted array
+//     vector<int> arr = {3, 5, 8, 15, 19};
+
+//     // Target value
+//     int x = 9;
+
+//     // Call the function
+//     int index = lowerBound(arr, x);
+
+//     cout << "The Lower Bound Index is: " << index << endl;
+
+//     return 0;
+// }
